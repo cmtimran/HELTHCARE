cb0e0ad209e1423abdeef675fba78b6fa23d1d3c
Prantho-das <pranthokumardas786@gmail.com>
2021-02-06 14:28:07 +0600

    Auth system by prantho

---
+++ Helthcare/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->integer('phone')->nullable();
+            $table->string('nid', 20)->nullable();
+            $table->string('role', 10)->default("PATIENT");
+            $table->string('avatar')->nullable();
+            $table->boolean('status')->default(true)->comment('1 means active & 0 means deactive');
+            $table->timestamp('last_log');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
